930f4acc48d891b81931991e12cdb8dc0fe17588
Jorn <Lotsch>
2024-02-19 19:59:47 +0100

    WIP

--- R/findBestImputation.R
+++ R/findBestImputation.R
@@ -5,135 +5,123 @@ library( abind )
 #################################### Functions ########################################################################
 
 # Function to rank matrices
-rankMEs <-
-  function( rankMx, inverted = FALSE ) {
-    if ( !inverted ) {
-      Mx <- lapply( rankMx, function( y ) apply( y, 2, function( x ) rank( x, na.last = TRUE ) ) )
-    } else {
-      Mx <- lapply( rankMx, function( y ) apply( y, 2, function( x ) rank( -x, na.last = FALSE ) ) )
-    }
-    return( Mx )
-  }
+rank_matrices <- function( rank_matrix, inverted = FALSE ) {
+  Mx <- lapply( rank_matrix, function( y ) apply( y, 2, function( x ) rank( ifelse( inverted, -x, x ), na.last = !inverted ) ) )
+  return( Mx )
+}
 
 # Function to calculate random rank scores
-calculateRandomRankScores <-
-  function( rankMx, totalPerm = 200 ) {
-    rankMxPerm <- rep( rankMx, round( totalPerm / length( rankMx ) ) )
-    rankMxPerm2 <- lapply( seq_along( rankMxPerm ), function( i ) {
-      set.seed( i )
-      rankMxPerm1 <- data.frame( apply( rankMxPerm[[i]], 2, function( x ) sample( x ) ) )
-      rownames( rankMxPerm1 ) <- rownames( rankMxPerm[[i]] )
-      return( rankMxPerm1 )
-    } )
-    return( rankMxPerm2 )
-  }
+calculate_random_rank_scores <- function( rank_matrix, total_perm = 200 ) {
+  rank_matrix_perm <- rep( rank_matrix, round( total_perm / length( rank_matrix ) ) )
+  rank_matrix_perm <- lapply( seq_along( rank_matrix_perm ), function( i ) {
+    set.seed( i )
+    rank_matrix_perm1 <- data.frame( apply( rank_matrix_perm[[i]], 2, function( x ) sample( x ) ) )
+    rownames( rank_matrix_perm1 ) <- rownames( rank_matrix_perm[[i]] )
+    return( rank_matrix_perm1 )
+  } )
+  return( rank_matrix_perm )
+}
 
 # Function to z-transform the ABC values
-renameDfcolumnsInNestedList <- function( df ) {
-  lapply( seq_along( df ), function( i ) {
-    y <- df[[i]]
+rename_df_columns_in_nested_list <- function( df_list ) {
+  lapply( seq_along( df_list ), function( i ) {
+    y <- df_list[[i]]
     colnames( y ) <- gsub( 'RMSE_', 'MeanRank_', colnames( y ) )
     return( y )
   } )
 }
 
 # Function to z-transform the ABC values
-calculateZABCvalues <- function( data, meanRanks ) {
-  nVar <- ncol( data[[1]] )
-  nMethods <- nrow( data[[1]] )
-  nTests <- length( data )
+calculate_z_abc_values <- function( data, mean_ranks ) {
+  n_var <- ncol( data[[1]] )
+  n_methods <- nrow( data[[1]] )
+  n_tests <- length( data )
 
-  d <- nVar * nTests
-  M <- nMethods
+  d <- n_var * n_tests
+  M <- n_methods
   m <- ( M + 1 ) / 2
-  s <- (1 / sqrt(12)) * ( M / sqrt(d) )
+  s <- ( 1 / sqrt( 12 ) ) * ( M / sqrt( d ) )
 
-  ZmeanRanksPerAlg <- ( meanRanks - m ) / s
-  ZmeanRanksPerAlg[ZmeanRanksPerAlg > 0] <- 0
-  ABCvalue <- ZmeanRanksPerAlg^2
+  z_mean_ranks_per_alg <- ( mean_ranks - m ) / s
+  z_mean_ranks_per_alg[z_mean_ranks_per_alg > 0] <- 0
+  abc_value <- z_mean_ranks_per_alg^2
 
-  return( ABCvalue )
+  return( abc_value )
 }
 
 # Function to calculate combined metrics
-calculateCombinedMetrics <-
-  function( RMSEMX, MEMx, rBiasMx ) {
+calculate_combined_metrics <- function( rmse_matrix, mem_matrix, rbias_matrix ) {
+  R_rmse_matrix <- rank_matrices( rmse_matrix )
+  R_mem_matrix <- rank_matrices( mem_matrix )
+  R_rbias_matrix <- rank_matrices( rbias_matrix )
 
-    RRMSEMX <- rankMEs( RMSEMX )
-    RMEMx <- rankMEs( MEMx )
-    RrBiasMx <- rankMEs( rBiasMx )
+  rank_errors_missings <- mapply( function( r1, r2, r3 ) {
+    ( r1 + r2 + r3 ) / 3
+  }, R_rmse_matrix, R_mem_matrix, R_rbias_matrix, SIMPLIFY = FALSE )
+  rank_errors_missings <- rename_df_columns_in_nested_list( df_list = rank_errors_missings )
 
-    rankErrorsMissings <- mapply( function( r1, r2, r3 ) {
-      ( r1 + r2 + r3 ) / 3 }  , RRMSEMX,  RMEMx, RrBiasMx, SIMPLIFY = FALSE )
-    rankErrorsMissings <- renameDfcolumnsInNestedList( df = rankErrorsMissings )
+  ranksums_errors_missings <- lapply( rank_errors_missings, function( x ) apply( x, 1, mean ) )
 
-      ranksumsErrorsMissings <- lapply( rankErrorsMissings, function( x ) apply( x, 1, mean ) )
+  a <- do.call( abind::abind, c( rank_errors_missings, list( along = 3 ) ) )
+  grand_mean_rank_errors_missings <- apply( a, 1:2, median )
 
-      a <- do.call( abind::abind, c( rankErrorsMissings, list( along = 3 ) ) )
-      grandMeanrankErrorsMissings <- apply( a, 1:2, median )
+  MajorityVoteRanksErrorsMissings <- lapply( ranksumsErrorsMissings, function( x ) names( which.min( x ) ) )
+  PerDatasetRanksums_Missings <- Reduce( "+", ranksumsErrorsMissings ) / length( ranksumsErrorsMissings )
+  BestPerDatasetRanksums_Missings <- which.min( PerDatasetRanksums_Missings )
+  zABCvalues <- calculateZABCvalues( data = RRMSEMX, meanRanks = PerDatasetRanksums_Missings )
+  ABCRanksums <-
+    ABCanalysis( zABCvalues )
+  BestRanksumsGrandMean_Missings_ABC_A <-
+    names( PerDatasetRanksums_Missings )[ABCRanksums$Aind]
 
-    MajorityVoteRanksErrorsMissings <- lapply( ranksumsErrorsMissings, function( x ) names( which.min( x ) ) )
-    PerDatasetRanksums_Missings <- Reduce( "+", ranksumsErrorsMissings ) / length( ranksumsErrorsMissings )
-    BestPerDatasetRanksums_Missings <- which.min( PerDatasetRanksums_Missings )
-    zABCvalues <- calculateZABCvalues( data = RRMSEMX, meanRanks = PerDatasetRanksums_Missings )
-    ABCRanksums <-
-      ABCanalysis( zABCvalues )
-    BestRanksumsGrandMean_Missings_ABC_A <-
-      names( PerDatasetRanksums_Missings )[ABCRanksums$Aind]
-
-    return( list(
-      rankErrorsMissings = rankErrorsMissings,
-      ranksumsErrorsMissings = ranksumsErrorsMissings,
-      MajorityVoteRanksErrorsMissings = MajorityVoteRanksErrorsMissings,
-      grandMeanrankErrorsMissings = grandMeanrankErrorsMissings,
-      PerDatasetRanksums_Missings = PerDatasetRanksums_Missings,
-      BestPerDatasetRanksums_Missings = BestPerDatasetRanksums_Missings,
-      zABCvalues = zABCvalues,
-      ABCRanksums = ABCRanksums,
-      BestRanksumsGrandMean_Missings_ABC_A = BestRanksumsGrandMean_Missings_ABC_A,
-      RRMSEMX = RRMSEMX,
-      RMEMx = RMEMx,
-      RrBiasMx = RrBiasMx
-    ) )
-  }
+  return( list(
+    rankErrorsMissings = rankErrorsMissings,
+    ranksumsErrorsMissings = ranksumsErrorsMissings,
+    MajorityVoteRanksErrorsMissings = MajorityVoteRanksErrorsMissings,
+    grandMeanrankErrorsMissings = grandMeanrankErrorsMissings,
+    PerDatasetRanksums_Missings = PerDatasetRanksums_Missings,
+    BestPerDatasetRanksums_Missings = BestPerDatasetRanksums_Missings,
+    zABCvalues = zABCvalues,
+    ABCRanksums = ABCRanksums,
+    BestRanksumsGrandMean_Missings_ABC_A = BestRanksumsGrandMean_Missings_ABC_A,
+    RRMSEMX = RRMSEMX,
+    RMEMx = RMEMx,
+    RrBiasMx = RrBiasMx
+  ) )
+}
 
 
 # Find best imputation
 
-BestMethod <-
-  pbmcapply::pbmclapply( DatasetNames, function( ActualDataset ) {
+BestMethod <- function( RepeatedSampleImputations ) {
+  # Extracting diagnostic missings
+  metrics_list <- lapply( RepeatedSampleImputations, function( x ) {
+    list(
+      RMSE = x[["ImputationRMSEInsertedMissings"]],
+      ME = x[["ImputationMEInsertedMissings"]],
+      rBias = x[["ImputationrBiasInsertedMissings"]]
+    )
+  } )
 
-    # Inserted diagnostic missings
+  # Calculate combined metrics
+  CombinedMetricsInsertedMissings <- calculateCombinedMetrics(
+    RMSEMX = lapply( metrics_list, function( x ) x$RMSE ),
+    MEMx = lapply( metrics_list, function( x ) x$ME ),
+    rBiasMx = lapply( metrics_list, function( x ) x$rBias )
+  )
 
-    RMSEinsertedMissings <- lapply( RepeatedSampleImputations, function( x ) {
-      x[["ImputationRMSEInsertedMissings"]]
-    } )
-    MEinsertedMissings <- lapply( RepeatedSampleImputations, function( x ) {
-      x[["ImputationMEInsertedMissings"]]
-    } )
-    rBiasInsertedMissings <- lapply( RepeatedSampleImputations, function( x ) {
-      x[["ImputationrBiasInsertedMissings"]]
-    } )
-
-    CombinedMetricsInsertedMissings <-
-      calculateCombinedMetrics( RMSEMX = RMSEinsertedMissings, MEMx = MEinsertedMissings, rBiasMx = rBiasInsertedMissings )
-
-    # Return results
-
-    return( list(
-      BestPerDatasetRanksums_insertedMissings = CombinedMetricsInsertedMissings[["BestPerDatasetRanksums_Missings"]],
-      BestRanksumsGrandMean_insertedMissings_ABC_A = CombinedMetricsInsertedMissings[["BestRanksumsGrandMean_Missings_ABC_A"]],
-      ranksumsErrorsInsertedMissings = CombinedMetricsInsertedMissings[["ranksumsErrorsMissings"]],
-      MajorityVoteRanksErrorsInsertedMissings = CombinedMetricsInsertedMissings[["MajorityVoteRanksErrorsMissings"]],
-      grandMeanrankErrorsInsertedMissings = CombinedMetricsInsertedMissings[["grandMeanrankErrorsMissings"]],
-      RMSEinsertedMissings = RMSEinsertedMissings,
-      MEinsertedMissings = MEinsertedMissings,
-      rBiasInsertedMissings = rBiasInsertedMissings,
-      ranksRMSEinsertedMissings = CombinedMetricsInsertedMissings[["RRMSEMX"]],
-      ranksMEinsertedMissings = CombinedMetricsInsertedMissings[["RMEMx"]],
-      ranksrBiasInsertedMissings = CombinedMetricsInsertedMissings[["RrBiasMx"]],
-      PerDatasetRanksums_insertedMissings = CombinedMetricsInsertedMissings[["PerDatasetRanksums_Missings"]],
-      zABCvalues_insertedMissings = CombinedMetricsInsertedMissings[["zABCvalues"]]
-
-    ) )
-  }, mc.cores = nProc )
+  # Return results
+  return( list(
+    BestPerDatasetRanksums_insertedMissings = CombinedMetricsInsertedMissings[["BestPerDatasetRanksums_Missings"]],
+    BestRanksumsGrandMean_insertedMissings_ABC_A = CombinedMetricsInsertedMissings[["BestRanksumsGrandMean_Missings_ABC_A"]],
+    ranksumsErrorsInsertedMissings = CombinedMetricsInsertedMissings[["ranksumsErrorsMissings"]],
+    MajorityVoteRanksErrorsInsertedMissings = CombinedMetricsInsertedMissings[["MajorityVoteRanksErrorsMissings"]],
+    grandMeanrankErrorsInsertedMissings = CombinedMetricsInsertedMissings[["grandMeanrankErrorsMissings"]],
+    RMSEinsertedMissings = metrics_list,
+    ranksRMSEinsertedMissings = CombinedMetricsInsertedMissings[["RRMSEMX"]],
+    ranksMEinsertedMissings = CombinedMetricsInsertedMissings[["RMEMx"]],
+    ranksrBiasInsertedMissings = CombinedMetricsInsertedMissings[["RrBiasMx"]],
+    PerDatasetRanksums_insertedMissings = CombinedMetricsInsertedMissings[["PerDatasetRanksums_Missings"]],
+    zABCvalues_insertedMissings = CombinedMetricsInsertedMissings[["zABCvalues"]]
+  ) )
+}
--- R/makeAndMeasureRepeatedImputations.R
+++ R/makeAndMeasureRepeatedImputations.R
@@ -8,16 +8,30 @@ library( parallel )
 
 # Function to impute data matrices with missing values
 imputeData <- function( dfMtx, dfMtxorig ) {
-  parallel::mclapply( ImputationMethods, function( method ) {
-    dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), makeBadImputations( dfMtx ) )
-    dfXmatriximputed_list <- data.frame( imputeMissings( x = dfMtx, method = method, imputationRepetitions = imputationRepetitions, seed = seed, x_orig = dfMtxorig ) )
+  if ( Sys.info( )["sysname"] != "Windows" ) {
 
-    if ( identical( dim( dfXmatriximputed_list ), dim( dfMtx ) ) ) {
-      dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), dfXmatriximputed_list )
-    }
+    parallel::mclapply( ImputationMethods, function( method ) {
+      dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), makeBadImputations( dfMtx ) )
+      dfXmatriximputed_list <- data.frame( imputeMissings( x = dfMtx, method = method, imputationRepetitions = imputationRepetitions, seed = seed, x_orig = dfMtxorig ) )
 
-    return( dfXmatriximputed )
-  }, mc.cores = nProc )
+      if ( identical( dim( dfXmatriximputed_list ), dim( dfMtx ) ) ) {
+        dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), dfXmatriximputed_list )
+      }
+
+      return( dfXmatriximputed )
+    }, mc.cores = nProc )
+  } else {
+    mclapply.windows( ImputationMethods, function( method ) {
+      dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), makeBadImputations( dfMtx ) )
+      dfXmatriximputed_list <- data.frame( imputeMissings( x = dfMtx, method = method, imputationRepetitions = imputationRepetitions, seed = seed, x_orig = dfMtxorig ) )
+
+      if ( identical( dim( dfXmatriximputed_list ), dim( dfMtx ) ) ) {
+        dfXmatriximputed <- cbind.data.frame( Data = paste0( method, " imputed" ), dfXmatriximputed_list )
+      }
+
+      return( dfXmatriximputed )
+    }, mc.cores = nProc )
+  }
 }
 
 # Function to calculate metrics for the imputations
@@ -41,6 +55,8 @@ makeMetricsMatrix <- function( OrigData, Missings_Which, ImputedData, Metric, Or
 
 # Function to insert diagnostic missing values and to perform the imputations
 makeAndMeasureRepeatedImputations <- function( Data, seeds, probMissing ) {
+  if ( Sys.info( )["sysname"] != "Windows" ) {
+
   rImputations <- pbmcapply::pbmclapply( seeds, function( seed ) {
     dfXmatrix <- Data
     dfXmatrixInitialMissings_Which <- lapply( seq_along( Data ), function( i ) which( is.na( Data[, i] ) ) )
@@ -116,4 +132,81 @@ makeAndMeasureRepeatedImputations <- function( Data, seeds, probMissing ) {
   }, mc.cores = nProc )
 
   return( rImputations )
+  } else {
+    rImputations <-  mclapply.windows( seeds, function( seed ) {
+      dfXmatrix <- Data
+      dfXmatrixInitialMissings_Which <- lapply( seq_along( Data ), function( i ) which( is.na( Data[, i] ) ) )
+      seedMissings <- seed
+      dfXmatrixInsertedMissings_WhichAndData <- createMissings( x = dfXmatrix, Prob = probMissing, seed = seedMissings, mnarity = 0, lowOnly = F, mnarshape = 1 )
+      iNA <- 1
+      MaxNAs <- max( apply( dfXmatrixInsertedMissings_WhichAndData$missData, 1, function( x ) sum( is.na( x ) ) ) )
+
+      while ( MaxNAs == ncol( dfXmatrixInsertedMissings_WhichAndData$missData ) ) {
+        dfXmatrixInsertedMissings_WhichAndData <- createMissings( x = dfXmatrix, Prob = probMissing, seed = seedMissings + 1000000 * iNA, mnarity = 0, lowOnly = F, mnarshape = 1 )
+        MaxNAs <- max( apply( dfXmatrixInsertedMissings_WhichAndData$missData, 1, function( x ) sum( is.na( x ) ) ) )
+        iNA <- iNA + 1
+      }
+
+      dfXmatrixInsertedMissings <- dfXmatrixInsertedMissings_WhichAndData$missData
+      dfXmatrixInsertedMissings_Which <- lapply( seq_along( dfXmatrixInsertedMissings_WhichAndData$toDelete ), function( i ) setdiff( dfXmatrixInsertedMissings_WhichAndData$toDelete[[i]], dfXmatrixInitialMissings_Which[[i]] ) )
+
+      # Impute data set
+      ImputedDataAll <- imputeData( dfMtx = dfXmatrixInsertedMissings, dfMtxorig = dfXmatrix )
+      names( ImputedDataAll ) <- ImputationMethods
+
+      # Combine imputed data set
+      dfImputedDataAll <- data.frame( do.call( rbind, ImputedDataAll ) )
+      dfXmatrixall <- rbind.data.frame(
+        cbind.data.frame( Data = "All data", dfXmatrix ),
+        cbind.data.frame( Data = "Missings", dfXmatrixInsertedMissings ),
+        dfImputedDataAll
+      )
+
+      # Calculate metrics
+      ImputationRMSEInsertedMissings <- makeMetricsMatrix(
+        OrigData = dfXmatrix,
+        Missings_Which = dfXmatrixInsertedMissings_Which,
+        ImputedData = dfImputedDataAll,
+        Metric = "RMSEImputedUnivar"
+      )
+      names( ImputationRMSEInsertedMissings ) <- paste0( "RMSE_", names( dfXmatrix ) )
+
+      ImputationMEInsertedMissings <- makeMetricsMatrix(
+        OrigData = dfXmatrix,
+        Missings_Which = dfXmatrixInsertedMissings_Which,
+        ImputedData = dfImputedDataAll,
+        Metric = "MEImputedUnivar"
+      )
+      names( ImputationMEInsertedMissings ) <- paste0( "ME_", names( dfXmatrix ) )
+
+      ImputationrBiasInsertedMissings <- makeMetricsMatrix(
+        OrigData = dfXmatrix,
+        Missings_Which = dfXmatrixInsertedMissings_Which,
+        ImputedData = dfImputedDataAll,
+        Metric = "rBiasImputedUnivar"
+      )
+      names( ImputationrBiasInsertedMissings ) <- paste0( "rBias_", names( dfXmatrix ) )
+
+      ImputationZDeltaInsertedMissings <- makeMetricsMatrix(
+        OrigData = dfXmatrix,
+        Missings_Which = dfXmatrixInsertedMissings_Which,
+        ImputedData = dfImputedDataAll,
+        Metric = "ZDelta",
+        OrigDataMiss = dfXmatrixInsertedMissings
+      )
+      names( ImputationZDeltaInsertedMissings ) <- paste0( "ZDelta_", names( dfXmatrix ) )
+
+      return( list(
+        dfXmatrixall = dfXmatrixall,
+        dfXmatrixInsertedMissings_Which = dfXmatrixInsertedMissings_Which,
+        ImputationRMSEInsertedMissings = ImputationRMSEInsertedMissings,
+        ImputationMEInsertedMissings = ImputationMEInsertedMissings,
+        ImputationrBiasInsertedMissings = ImputationrBiasInsertedMissings,
+        ImputationZDeltaInsertedMissings = ImputationZDeltaInsertedMissings
+      ) )
+
+    }, mc.cores = nProc )
+
+    return( rImputations )
+  }
 }
